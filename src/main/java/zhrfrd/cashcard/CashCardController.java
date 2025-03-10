09962b8a8bbdc7603b8e7d54c2a3c740e4cf38fb
Farid Zouheir <zouheirjob@hotmail.com>
2025-03-10 14:54:22 +0000

    Refactor CashCard API to use in-memory H2 database to populate data

--- src/main/java/zhrfrd/cashcard/CashCardController.java
+++ src/main/java/zhrfrd/cashcard/CashCardController.java
@@ -6,14 +6,23 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController   // Indicates that this is component able to handle HTTP requests
 @RequestMapping("/cashcards")   // Indicates which address requests must have to access this component
 class CashCardController {
-    @GetMapping("/{requestedId}")   // Indicates that this is a handler method. More in specific GET requests that match /cashcards/{requestedId} will be handled by this method
-    private ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {   // @PathVariable makes Spring Web aware of the requestedId supplied in the HTTP request. Now it’s available for us to use in our handler method.
-        if (requestedId.equals(99L)) {
-            CashCard cashCard = new CashCard(99L, 123.45);
-            return ResponseEntity.ok(cashCard);
+    private final CashCardRepository cashCardRepository;
+
+    private CashCardController(CashCardRepository cashCardRepository) {
+        this.cashCardRepository = cashCardRepository;
+    }
+
+    @GetMapping("/{requestedId}")
+    private ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
+        Optional<CashCard> cashCardOptional = cashCardRepository.findById(requestedId);
+
+        if (cashCardOptional.isPresent()) {
+            return ResponseEntity.ok(cashCardOptional.get());
         } else {
             return ResponseEntity.notFound().build();
         }
---
+++ src/main/java/zhrfrd/cashcard/CashCardRepository.java
@@ -0,0 +1,6 @@
+package zhrfrd.cashcard;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CashCardRepository extends CrudRepository<CashCard, Long> {
+}
